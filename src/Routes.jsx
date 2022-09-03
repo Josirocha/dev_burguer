59f2de6bfcbab4468aaec11c5425d0d051e4763e
Josiane Rocha <josi1992@gmail.com>
2022-09-02 23:39:39 -0300

    feat: change material ui Theme

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,21 +1,38 @@
 import React from "react";
 import Login from "./Pages/Login/Login";
 import { BrowserRouter, Routes as Switch, Route } from "react-router-dom";
+import Header from "./Components/Common/Header/Header";
+import { createTheme, ThemeProvider } from '@mui/material';
+import Cardapio from "./Pages/Cardapio/Cardapio";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#004643'
+    },
+    secondary: {
+      main: '#ffffff'
+    },
+  },
+});
 
 const Routes = () => {
   return (
-    <BrowserRouter>
-      <Switch>
-        {/* home */}
-        <Route />
-        {/* login */}
-        <Route path="login" element={<Login />} />
-        {/* cadastro */}
-        <Route />
-        {/* cardapio */}
-        <Route />
-      </Switch>
-    </BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <Header />
+        <Switch>
+          {/* home */}
+          <Route />
+          {/* login */}
+          <Route path="login" exact element={<Login />} />
+          {/* cadastro */}
+          <Route />
+          {/* cardapio */}
+          <Route path="cardapio" exact element={<Cardapio />} />
+        </Switch>
+      </BrowserRouter>
+    </ThemeProvider>
   );
 };
 
