667f87e933d1a31a71d92658c824a94a04ac093b
Josiane Rocha <josi1992@gmail.com>
2022-09-07 20:53:27 -0300

    feat: add edit and delete button

--- src/Components/CardCardapio/CardCardapio.jsx
+++ src/Components/CardCardapio/CardCardapio.jsx
@@ -6,28 +6,40 @@ import {
     Button,
     Typography,
 } from "@mui/material";
-import AddShoppingCartIcon from "@mui/icons-material/AddShoppingCart";
+import CreateOutlinedIcon from "@mui/icons-material/CreateOutlined";
+import DeleteOutlineOutlinedIcon from "@mui/icons-material/DeleteOutlineOutlined";
 import React from "react";
 
 const CardCardapio = ({ img, name, ingredientes, valor }) => {
     return (
         <Card
             sx={{
-                maxWidth: 345
+                maxWidth: 345,
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "space-between",
             }}
         >
-            <CardMedia CardMedia component="img" height="280" image={img} />
+            <div>
+                <CardMedia CardMedia component="img" height="280" image={img} />
 
-            <CardContent>
-                <Typography gutterBottom variante="h5" component="div">
-                    {name}
-                </Typography>
+                <CardContent>
+                    <Typography gutterBottom variante="h5" component="div">
+                        {name}
+                    </Typography>
 
-                <Typography variant="body2">{ingredientes}</Typography>
-                <Typography variant="p">{valor}</Typography>
-            </CardContent>
-            <CardActions>
-                <Button startIcon={<AddShoppingCartIcon />}>Comprar</Button>
+                    <Typography variant="body2">{ingredientes}</Typography>
+                    <Typography variant="p">{valor}</Typography>
+                </CardContent>
+            </div>
+
+            <CardActions
+                sx={{ display: "flex", justifyContent: "space-between" }}
+            >
+                <Button startIcon={<CreateOutlinedIcon />}>Editar</Button>
+                <Button startIcon={<DeleteOutlineOutlinedIcon />}>
+                    Deletar
+                </Button>
             </CardActions>
         </Card>
     );
